3cba8a779128e4fa51b7b7da561e9f4fc15a42c9
Julian Pompey <jupompey00@gmail.com>
2024-05-06 10:07:47 -0400

    basic html setup

---
+++ scripts/overnightCostEstimator.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const nameTextBox = document.getElementById("nameTextBox");
+const emailTextBox = document.getElementById("emailTextBox");
+const checkInDateTextBox = document.getElementById("checkInDateTextBox");
+
+
+window.onload = function (){
+
+}
